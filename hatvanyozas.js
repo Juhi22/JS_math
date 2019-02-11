1d235744f3ac5a5863d1d25c02d265474b3f27ec
Juhi22 <j.dani22@freemail.hu>
2019-02-11 15:34:03 +0100

    hatvanyozas es foprogram

--- hatvanyozas.js
+++ hatvanyozas.js
@@ -1,5 +1,9 @@
 const hatvanyozas = (a, b) => {
-  return Math.pow(a, b);
+  let result = a;
+  for (let i = 0; i < b; i++) {
+    result = result * a;
+  }
+  return result;
 };
 
 module.exports = { hatvanyozas };
