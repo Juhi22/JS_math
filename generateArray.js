93c97c4526a98d6c35d8d96cc39403f6a6332c48
micsikz <micsikzoltan@vipmail.hu>
2019-02-11 21:01:09 +0100

    Dani készítette

---
+++ generateArray.js
@@ -0,0 +1,10 @@
+// 1-20 között ad 10 random számot
+const generateArray = (mennyinum) => {
+  let result = [];
+  for (let i = 0; i < mennyinum; i++) {
+    let random = Math.floor(Math.random() * 20) + 1;
+    result.push(random);
+  }
+  return result;
+};
+module.exports = { generateArray };
